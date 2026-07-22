193e66d194fb125f5a809acaf6bb0c4d3b7c7289
Nico Nößler <nico.noessler@dynatrace.com>
2026-07-22 16:24:30 +0200

    disable helm chart signing when no secret is set

--- hack/build/ci/generate-helm-package.sh
+++ hack/build/ci/generate-helm-package.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-mkdir -p ~/.gnupg
-echo "${SECRING}" | base64 -d >~/.gnupg/secring.gpg
-echo "${PASSPHRASE}" >~/.gnupg/passphrase
+if [[ -n "${SECRING}" ]]; then
+    mkdir -p ~/.gnupg
+    echo "${SECRING}" | base64 -d >~/.gnupg/secring.gpg
+    echo "${PASSPHRASE}" >~/.gnupg/passphrase
 
-cleanup() {
-    rm -f ~/.gnupg/secring.gpg
-    rm -f ~/.gnupg/passphrase
-}
+    cleanup() {
+        rm -f ~/.gnupg/secring.gpg
+        rm -f ~/.gnupg/passphrase
+    }
 
-trap 'cleanup' ERR
+    trap 'cleanup' ERR
 
-helm package \
-    "./config/helm/chart/default/" \
-    -d "${OUTPUT_DIR}" \
-    --app-version "${VERSION_WITHOUT_PREFIX}" \
-    --version "${VERSION_WITHOUT_PREFIX}" \
-    --sign \
-    --key "Dynatrace LLC" \
-    --keyring ~/.gnupg/secring.gpg \
-    --passphrase-file ~/.gnupg/passphrase
+    helm package \
+        "./config/helm/chart/default/" \
+        -d "${OUTPUT_DIR}" \
+        --app-version "${VERSION_WITHOUT_PREFIX}" \
+        --version "${VERSION_WITHOUT_PREFIX}" \
+        --sign \
+        --key "Dynatrace LLC" \
+        --keyring ~/.gnupg/secring.gpg \
+        --passphrase-file ~/.gnupg/passphrase
+else
+    helm package \
+        "./config/helm/chart/default/" \
+        -d "${OUTPUT_DIR}" \
+        --app-version "${VERSION_WITHOUT_PREFIX}" \
+        --version "${VERSION_WITHOUT_PREFIX}"
+fi
